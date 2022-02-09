a45df1e9ae45fcffd19ce346eb737c1f6875379f
Neven <osiris@Osireion.local>
2022-02-08 21:32:19 -0800

    bcrypt test commit

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 const _ = require('lodash');
-const encrypt = require('mongoose-encryption')
+// const encrypt = require('mongoose-encryption');
+// const md5 = require('md5');
+// const bcrypt = require('bcrypt');
+// const saltRounds = 10;
 
 const app = express();
 
@@ -20,7 +23,7 @@ const userSchema = new mongoose.Schema({
 	password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
 
 // Make sure encryption is done before storing data on DB
 const User = mongoose.model("User", userSchema);
@@ -39,39 +42,57 @@ app.get("/register", function(req, res) {
 
 app.post("/register", function(req, res) {
 
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
 
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  })
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+
+  });
 
 });
 
 app.post("/login", function(req, res) {
+
   const username = req.body.username;
   const password = req.body.password;
 
     User.findOne({email: username}, function (err, foundUser) {
+
       if (err) {
         res.send("No user found");
       } else {
+
         if (foundUser) {
-          if (foundUser.password === password) {
-            res.render("secrets");
-          } else {
-            res.send("Wrong password");
-          }
+
+          bcrypt.compare(password, foundUser.password, function(error, result) {
+
+            if (result === true) {
+              res.render("secrets");
+            } else {
+              res.send("wrong password");
+            }
+
+          });
+
         }
+
       }
-    })
+
+    });
+
 });
+
+
 
 let port = process.env.PORT;
 if (port == null || port == "") {
